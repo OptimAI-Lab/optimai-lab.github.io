311a3b6738ff03437ed98116dc6aa6934adab9a0
jiajunruan <jiajunruan@outlook.com>
2026-05-11 11:14:38 +0000

    Deploying to gh-pages from @ OptimAI-Lab/optimai-lab.github.io@298a9355acc5cac6b60a08c61ed14d0b18d8746e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,56 +412,376 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-news-10",
-          title: 'News_10',
+            },},{id: "news-2014-01-promotion",
+          title: '2014 01 Promotion',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/news_10/";
-            },},{id: "news-news-09",
-          title: 'News_09',
+              window.location.href = "/news/2014-01-promotion/";
+            },},{id: "news-2014-05-bs-power",
+          title: '2014 05 Bs Power',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/news_09/";
-            },},{id: "news-news-08",
-          title: 'News_08',
+              window.location.href = "/news/2014-05-bs-power/";
+            },},{id: "news-2014-06-5g-survey",
+          title: '2014 06 5g Survey',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/news_08/";
-            },},{id: "news-news-07",
-          title: 'News_07',
+              window.location.href = "/news/2014-06-5g-survey/";
+            },},{id: "news-2014-08-isu-start",
+          title: '2014 08 Isu Start',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/news_07/";
-            },},{id: "news-news-06",
-          title: 'News_06',
+              window.location.href = "/news/2014-08-isu-start/";
+            },},{id: "news-2014-09-parallel",
+          title: '2014 09 Parallel',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/news_06/";
-            },},{id: "news-news-05",
-          title: 'News_05',
+              window.location.href = "/news/2014-09-parallel/";
+            },},{id: "news-2014-09-fellowship",
+          title: '2014 09 Fellowship',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/news_05/";
-            },},{id: "news-news-04",
-          title: 'News_04',
+              window.location.href = "/news/2014-09-fellowship/";
+            },},{id: "news-2015-08-nsf-ccf",
+          title: '2015 08 Nsf Ccf',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/news_04/";
-            },},{id: "news-news-03",
-          title: 'News_03',
+              window.location.href = "/news/2015-08-nsf-ccf/";
+            },},{id: "news-2015-08-spm",
+          title: '2015 08 Spm',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/news_03/";
-            },},{id: "news-news-02",
-          title: 'News_02',
+              window.location.href = "/news/2015-08-spm/";
+            },},{id: "news-2015-09-haoran",
+          title: '2015 09 Haoran',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/news_02/";
-            },},{id: "news-news-01",
-          title: 'News_01',
+              window.location.href = "/news/2015-09-haoran/";
+            },},{id: "news-2015-11-admm-nonconvex",
+          title: '2015 11 Admm Nonconvex',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/news_01/";
+              window.location.href = "/news/2015-11-admm-nonconvex/";
+            },},{id: "news-2015-12-icassp",
+          title: '2015 12 Icassp',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2015-12-icassp/";
+            },},{id: "news-2016-01-async-admm",
+          title: '2016 01 Async Admm',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2016-01-async-admm/";
+            },},{id: "news-2016-05-admm-linear",
+          title: '2016 05 Admm Linear',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2016-05-admm-linear/";
+            },},{id: "news-2016-06-moa",
+          title: '2016 06 Moa',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2016-06-moa/";
+            },},{id: "news-2016-08-iccopt",
+          title: '2016 08 Iccopt',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2016-08-iccopt/";
+            },},{id: "news-2016-08-nestt",
+          title: '2016 08 Nestt',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2016-08-nestt/";
+            },},{id: "news-2016-11-admm",
+          title: '2016 11 Admm',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2016-11-admm/";
+            },},{id: "news-2017-01-spcom",
+          title: '2017 01 Spcom',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2017-01-spcom/";
+            },},{id: "news-2017-01-snmf",
+          title: '2017 01 Snmf',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2017-01-snmf/";
+            },},{id: "news-2017-04-songtao-award",
+          title: '2017 04 Songtao Award',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2017-04-songtao-award/";
+            },},{id: "news-2017-05-icml",
+          title: '2017 05 Icml',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2017-05-icml/";
+            },},{id: "news-2017-08-umn-move",
+          title: '2017 08 Umn Move',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2017-08-umn-move/";
+            },},{id: "news-2017-08-nsf-cmmi",
+          title: '2017 08 Nsf Cmmi',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2017-08-nsf-cmmi/";
+            },},{id: "news-2017-11-mlsp",
+          title: '2017 11 Mlsp',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2017-11-mlsp/";
+            },},{id: "news-2017-11-davood-defense",
+          title: '2017 11 Davood Defense',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2017-11-davood-defense/";
+            },},{id: "news-2018-02-ziping",
+          title: '2018 02 Ziping',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2018-02-ziping/";
+            },},{id: "news-2018-03-mmwave",
+          title: '2018 03 Mmwave',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2018-03-mmwave/";
+            },},{id: "news-2018-04-topic",
+          title: '2018 04 Topic',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2018-04-topic/";
+            },},{id: "news-2018-05-gradient-pd",
+          title: '2018 05 Gradient Pd',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2018-05-gradient-pd/";
+            },},{id: "news-2018-05-learning-optimize",
+          title: '2018 05 Learning Optimize',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2018-05-learning-optimize/";
+            },},{id: "news-2018-09-multi-agent",
+          title: '2018 09 Multi Agent',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2018-09-multi-agent/";
+            },},{id: "news-2018-09-actor-critic",
+          title: '2018 09 Actor Critic',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2018-09-actor-critic/";
+            },},{id: "news-2018-10-asilomar",
+          title: '2018 10 Asilomar',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2018-10-asilomar/";
+            },},{id: "news-2018-11-globalsip",
+          title: '2018 11 Globalsip',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2018-11-globalsip/";
+            },},{id: "news-2018-12-signsgd",
+          title: '2018 12 Signsgd',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2018-12-signsgd/";
+            },},{id: "news-2018-12-adam",
+          title: '2018 12 Adam',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2018-12-adam/";
+            },},{id: "news-2019-03-aro",
+          title: '2019 03 Aro',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2019-03-aro/";
+            },},{id: "news-2019-03-xinwei",
+          title: '2019 03 Xinwei',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2019-03-xinwei/";
+            },},{id: "news-2019-04-gradient",
+          title: '2019 04 Gradient',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2019-04-gradient/";
+            },},{id: "news-2019-06-distributed",
+          title: '2019 06 Distributed',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2019-06-distributed/";
+            },},{id: "news-2019-06-battery",
+          title: '2019 06 Battery',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2019-06-battery/";
+            },},{id: "news-2019-07-nsf",
+          title: '2019 07 Nsf',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2019-07-nsf/";
+            },},{id: "news-2019-08-afosr",
+          title: '2019 08 Afosr',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2019-08-afosr/";
+            },},{id: "news-2019-09-neurips",
+          title: '2019 09 Neurips',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2019-09-neurips/";
+            },},{id: "news-2019-11-princeton",
+          title: '2019 11 Princeton',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2019-11-princeton/";
+            },},{id: "news-2019-12-hybrid-block",
+          title: '2019 12 Hybrid Block',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2019-12-hybrid-block/";
+            },},{id: "news-2020-06-minmax-overview",
+          title: '2020 06 Minmax Overview',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2020-06-minmax-overview/";
+            },},{id: "news-2020-06-penalty",
+          title: '2020 06 Penalty',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2020-06-penalty/";
+            },},{id: "news-2020-06-icml",
+          title: '2020 06 Icml',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2020-06-icml/";
+            },},{id: "news-2020-07-nsf-intel",
+          title: '2020 07 Nsf Intel',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2020-07-nsf-intel/";
+            },},{id: "news-2020-07-ibm-award",
+          title: '2020 07 Ibm Award',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2020-07-ibm-award/";
+            },},{id: "news-2020-08-prashant",
+          title: '2020 08 Prashant',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2020-08-prashant/";
+            },},{id: "news-2020-08-iccm-award",
+          title: '2020 08 Iccm Award',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2020-08-iccm-award/";
+            },},{id: "news-2020-09-songtao-ibm",
+          title: '2020 09 Songtao Ibm',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2020-09-songtao-ibm/";
+            },},{id: "news-2020-09-neurips",
+          title: '2020 09 Neurips',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2020-09-neurips/";
+            },},{id: "news-2020-10-mimo",
+          title: '2020 10 Mimo',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2020-10-mimo/";
+            },},{id: "news-2020-12-hybrid-fl",
+          title: '2020 12 Hybrid Fl',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2020-12-hybrid-fl/";
+            },},{id: "news-2020-12-a3c",
+          title: '2020 12 A3c',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2020-12-a3c/";
+            },},{id: "news-2021-02-best-paper",
+          title: '2021 02 Best Paper',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2021-02-best-paper/";
+            },},{id: "news-2021-04-wireless",
+          title: '2021 04 Wireless',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2021-04-wireless/";
+            },},{id: "news-2021-05-riemannian",
+          title: '2021 05 Riemannian',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2021-05-riemannian/";
+            },},{id: "news-2021-05-rmsprop",
+          title: '2021 05 Rmsprop',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2021-05-rmsprop/";
+            },},{id: "news-2021-05-saddle",
+          title: '2021 05 Saddle',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2021-05-saddle/";
+            },},{id: "news-2021-05-bilevel",
+          title: '2021 05 Bilevel',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2021-05-bilevel/";
+            },},{id: "news-2021-06-stem",
+          title: '2021 06 Stem',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2021-06-stem/";
+            },},{id: "news-2022-05-xiangyi-graduation",
+          title: '2022 05 Xiangyi Graduation',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2022-05-xiangyi-graduation/";
+            },},{id: "news-2022-10-hiking",
+          title: '2022 10 Hiking',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2022-10-hiking/";
+            },},{id: "news-2022-12-escape-room",
+          title: '2022 12 Escape Room',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2022-12-escape-room/";
+            },},{id: "news-2023-09-kayak",
+          title: '2023 09 Kayak',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-09-kayak/";
+            },},{id: "news-2023-11-xinwei-defense",
+          title: '2023 11 Xinwei Defense',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-11-xinwei-defense/";
+            },},{id: "news-2023-11-xinwei-dinner",
+          title: '2023 11 Xinwei Dinner',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-11-xinwei-dinner/";
+            },},{id: "news-2023-12-neurips",
+          title: '2023 12 Neurips',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2023-12-neurips/";
+            },},{id: "news-2024-09-kayak",
+          title: '2024 09 Kayak',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2024-09-kayak/";
+            },},{id: "news-2025-05-siliang-defense",
+          title: '2025 05 Siliang Defense',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2025-05-siliang-defense/";
             },},{id: "projects-large-scale-optimization",
           title: 'Large-Scale Optimization',
           description: "Design and analysis of modern optimization algorithms for large-scale problems",
