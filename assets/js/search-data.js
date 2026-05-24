ad62f997bcfd5cbae690056bd45a179e9107f78e
Mingyi-Hong <67966678+Mingyi-Hong@users.noreply.github.com>
2026-05-24 00:28:43 +0000

    Deploying to gh-pages from @ OptimAI-Lab/optimai-lab.github.io@a220122c558f1541fc0d87f0412b263017b0e6eb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1065,6 +1065,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2026-05-talk/";
+            },},{id: "news-2026-05-dawei-postdoc",
+          title: '2026 05 Dawei Postdoc',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/2026-05-dawei-postdoc/";
             },},{id: "projects-leak-k",
           title: 'Leak@k',
           description: "A meta-metric evaluation framework to evaluate robustness of LLM unlearning",
